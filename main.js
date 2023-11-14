20b6d40747a36eca0ba31424227c730e4538ab7a
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-11-14 16:21:36 +0000

    Finish todo logic
    I think the logic/constructors themselves are done, I just need to work
    on the design and then HTML/DOM manipulation.
    
    I want to test out some things first.

--- main.js
+++ main.js
@@ -1,5 +1,133 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   createProject: () => (/* binding */ createProject),
+/* harmony export */   createTask: () => (/* binding */ createTask),
+/* harmony export */   defaultProject: () => (/* binding */ defaultProject)
+/* harmony export */ });
+class createTask {
+    constructor(title,description,dueDate,priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.complete = false;  
+    }
+    toggleComplete(){
+        switch(this.complete){
+            case true:
+                this.complete = false;
+                break;
+            case false:
+                this.complete = true;
+                break;
+        }
+    }
+}
+
+class createProject {
+    constructor(name){
+        this.name = name;
+        this.tasks = [];
+    }
+    createTask(title,description,dueDate,priority){
+        this.tasks.push(new createTask(title,description,dueDate,priority));
+    }
+};
+
+//created new project example
+let defaultProject = new createProject("Example Project");
+//created new task example
+defaultProject.createTask("Example Task", "This is just an example","16-11-2023","Low");
+//set task as complete example
+defaultProject.tasks[0].toggleComplete();
+//edit task title example
+defaultProject.tasks[0].title = "not example"
+// //edit task description example
+defaultProject.tasks[0].description = "this is not an example"
+// //edit task due date example
+defaultProject.tasks[0].dueDate = "18-11-2023"
+// //edit task priority example
+defaultProject.tasks[0].priority = "High"
+//delete task example
+defaultProject.tasks.splice(0,1);
+defaultProject.createTask("Second Task", "This is just a second example example","16-11-2023","High");
+
+
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _todo_logic_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+
+
+console.log(_todo_logic_js__WEBPACK_IMPORTED_MODULE_0__.defaultProject);
 console.log("test");
+})();
+
 /******/ })()
 ;
